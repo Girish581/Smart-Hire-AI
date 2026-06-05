0f72f86d5a5e6a7a449837b6db65419d2f6bdcbf
Girish Mahajan <girishmahajan581@gmail.com>
2026-06-05 12:42:46 +0530

    Initial File Uploads

---
+++ src/main/java/com/SmartHire/smart_hire/SmartHireApplication.java
@@ -0,0 +1,13 @@
+package com.SmartHire.smart_hire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHireApplication.class, args);
+	}
+
+}
